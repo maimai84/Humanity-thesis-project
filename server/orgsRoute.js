c842d4fb00db9d5a0cc8ae71d529dcfe79e52fb8
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-10-18 20:21:20 +0300

    (modify) database online now

--- server/orgsRoute.js
+++ server/orgsRoute.js
@@ -1,14 +1,53 @@
 const Orgs = require('../database/comp/orgs.js');
 
 module.exports = {
-	get : {
-		'/someEndPoint' : (req, res) => {
-
-		}
-	},
-	post : {
-		'/anotherEndPoint' : (req, res) => {
-
-		}
-	}
+  get : {
+    '/' : (req, res, cb) => {
+      Orgs.find({})
+        .then((data) => {
+          cb(data);
+        })
+        .catch((err) => {
+          console.log('error getting Orgs : ' , err);
+          cb([]);
+        })
+    },
+    '/signout' : (req, res, cb) => {
+      req.session.destroy((err) => {
+        if (err) {
+          console.log('error destroying session !!');
+          cb(false);
+        } else {
+          cb(true);
+        }
+      })
+    }
+  },
+  post : {
+    '/signin' : (req, res, cb) => {
+      var info = req.body;
+      //check db for org 
+      Orgs.find({where : {name : info.name , password : info.password}})
+        .then((user) => {
+          console.log('signing in for : ', user.get('name'));
+          cb(!!user);
+        })
+        .catch((err) => {
+          cb(false);          
+        })
+    },
+    '/signup' : (req, res, cb) => {
+      var info = req.body;
+      console.log('info : ', info);
+      Orgs.build(info)
+        //add info to db ..
+        .save()
+        .then(() => {
+          cb(`recieved org : ${info} and saved`);
+        })
+        .catch(() => {
+          cb(`recieved org : ${info} but not saved `);
+        })      
+    }
+  }
 }
