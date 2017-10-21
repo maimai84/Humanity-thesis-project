a56d9f2213e94f4b4e19a6531ea2da4fb25ca38c
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-10-21 18:47:42 +0300

    ;components editing

--- client/userprofile.js
+++ client/userprofile.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import allStyle from './style.js';
 
 const styles = StyleSheet.create(allStyle.userprofile);
 
