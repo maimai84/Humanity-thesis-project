80777535e329df7e48bcd84619d5b97563766bf7
maimai84 <maialmasalmah@gmail.com>
2017-10-22 19:44:56 +0300

    map the event

--- client/eventstodo.js
+++ client/eventstodo.js
@@ -1,8 +1,45 @@
 import React from "react";
-import {  View,StyleSheet } from 'react-native'
+import {  View, StyleSheet, Text } from 'react-native'
 
 // import Entryepage from './client/Entryepage';
 import Entryevent from './Entryevent'
+
+export default class List extends React.Component {
+  constructor(){
+    super()
+      this.state ={current:false,eventlist:[]};
+      this.getEvents = () =>{
+      fetch('https://thawing-garden-23809.herokuapp.com/events',{'method':'GET',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        },
+      }).then((response) => {
+
+      let body = response.json();
+      this.setState({eventlist : body._bodyInit});
+      console.log(this.state.eventlist)
+      })
+      }
+
+            
+  }
+
+   
+  render(){
+    return (
+       <View style={styles.container}>
+       <Text>rrrr</Text>
+       {this.getEvents.bind(this)}
+          {
+               this.state.eventlist.map((event) => (<Entryevent event = {event} /> ))
+          } 
+          
+       </View>
+    )
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -14,34 +51,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#F8F8F8',
   }
 });
-export default class List extends React.Component {
-  constructor(){
-    super()
-      this.state ={current:false,eventlist:[]} ;
-    
-      fetch('https://thawing-garden-23809.herokuapp.com/events',{'method':'GET',
-         headers: {
-         'Accept': 'application/json',
-         'Content-Type': 'application/json',
-         },
-      }).then((response) => {
-
-      let body = response.json();
-      this.setState({eventlist : body._bodyInit});
-      });
-
-            
-  }
-
-   
-  render(){
-    return (
-       <View style={styles.container}> 
-          {
-               this.state.eventlist.map((event) => (<Entryevent event = {event.id} /> ))
-          } 
-       </View>
-    )
-  }
-}
 
