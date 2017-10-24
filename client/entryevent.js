0e2d39cf2a52806b44b2ac1317267782ba668e37
maimai84 <maialmasalmah@gmail.com>
2017-10-24 18:46:23 +0300

    style

--- client/entryevent.js
+++ client/entryevent.js
@@ -1,47 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import { Text, View,TouchableOpacity,StyleSheet} from 'react-native';
+import { Text, View,TouchableOpacity,StyleSheet} from "react-native";
 export default class Entryevent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return(
-      <View style = {styles.cont}>
-            <TouchableOpacity>
-               <Text style = {styles.container1}>{this.props.event.name}</Text>
-               <Text style = {styles.container} >{this.props.event.description}</Text>
-               <Text style = {styles.container}>{this.props.event.location}</Text>
-               <Text style = {styles.container}>{this.props.event.org_id}</Text>
-            </TouchableOpacity> 
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <View style = {styles.cont}>
+                <TouchableOpacity>
+                    <Text style = {styles.container1}>{this.props.event.name}</Text>
+                    <Text style = {styles.container} >{this.props.event.description}</Text>
+                    <Text style = {styles.container}>{this.props.event.location}</Text>
+                    <Text style = {styles.container}>{this.props.event.org_id}</Text>
+                </TouchableOpacity> 
              
-      </View>
-    )
-  }
+            </View>
+        );
+    }
 }
-const styles = StyleSheet.create({
-
-  container: {
-    
-  fontSize: 14,
-  lineHeight: 24,
-  color: 'black'
-   
-  },
-  container1: {
-    
-  fontSize: 14,
-  lineHeight: 24,
-  color: 'blue'
-   
-  },
-   cont: {
- backgroundColor:"#99ceff",
- width:350,
- marginLeft:0,
- marginRight:0
-     
- //    justifyContent: 'space-between'
-  }
-});
+const styles = StyleSheet.create(
+    {
+        container: { 
+            fontSize: 14,
+            lineHeight: 24,
+            color: "black"
+        },
+        container1: {  
+            fontSize: 14,
+            lineHeight: 24,
+            color: "blue"
+        },
+        cont: {
+            backgroundColor:"#99ceff",
+            width:350,
+            marginLeft:0,
+            marginRight:0  
+        }
+    }
+);
 
--- client/userprofile.js
+++ client/userprofile.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import allStyle from './style.js';
+import React from "react";
+import { Text, View} from "react-native";
+// import allStyle from "./style.js";
 // const styles = StyleSheet.create(allStyle.userprofile);
 
 export default class Userprofile extends React.Component {
-  constructor(props) {
-      super(props);
+    constructor(props) {
+        super(props);
 
-  }
-  profile() {
-    console.log(this.props)
-    if(this.props.userprofile) {
-      return  (<View>
-                <Text>
-                  {this.props.userprofile.username}
-                </Text>
-                <Text>
-                  {this.props.userprofile.email}
-                 </Text>
-                 <Text> 
-                  {this.props.userprofile.rate}
-                </Text>
-              </View>)
     }
-    return <Text>not signed in or no props passeed</Text>;
-  }
+    profile() {
+        
+        if(this.props.userprofile) {
+            return  (<View>
+                <Text>
+                    {this.props.userprofile.username}
+                </Text>
+                <Text>
+                    {this.props.userprofile.email}
+                </Text>
+                <Text> 
+                    {this.props.userprofile.rate}
+                </Text>
+            </View>);
+        }
+        return <Text>not signed in or no props passeed</Text>;
+    }
 
 
-  render() {
-    return (
-      <View>
-        {this.profile()}
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                {this.profile()}
+            </View>
+        );
+    }
 }
 
 // if(this.props.userprofile.userevent){
