982cf615d17f57d5c2a1a570a2aba123077c77bf
maimai84 <maialmasalmah@gmail.com>
2017-11-04 11:14:15 +0200

    add join Button

--- client/EventPage.js
+++ client/EventPage.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, Image} from 'react-native';
-
+import { Text, View, Alert, TouchableOpacity, StyleSheet, Image,Button} from 'react-native';
+import conf from '../config.js';
 export default class EventPage extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); 
+        this.state = {
+      
+        event: props.event,
+        
+        joined:false
+        };
   }
+  myFunctions(){
+      this.onJoin();
+      this.setState({joined: true});
+  }
+
+  onJoin() {
+      fetch(conf.url + '/events/join', {
+      method: 'POST',
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+  },
+      body: JSON.stringify({
+      id: this.state.event.id,
+      org_id:this.state.event.org_id
+  })
+  })
+ .then((response) => response.json())
+      .then((data) => {
+       console.log(data)
+       Alert.alert( 'JOIN ', data.message, [{text: 'OK', onPress: () => console.log('OK Pressed')}, ], { cancelable: true } )
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+}
   render() {
     return(
       <View>
-      <Image source={require("../images/blue.jpg")} >
+       <Image source={require("../images/blue.jpg")} >
         <TouchableOpacity >
-           <Text>{this.props.event.name}</Text>
-           <Text>{this.props.event.description}</Text>
-           <Text>{this.props.event.location}</Text>
-           <Text>{this.props.event.time}</Text>
+        <Text>{this.props.event.name}</Text>
+        <Text>{this.props.event.description}</Text>
+        <Text>{this.props.event.location}</Text>
+        <Text>{this.props.event.time}</Text>
+          <Button title="join" onPress = {this.myFunctions.bind(this)} />
         </TouchableOpacity> 
         </Image>
       </View>
