3aea0527daca028f1d10786d2273d5adda1fd621
maimai84 <maialmasalmah@gmail.com>
2017-11-08 15:00:44 +0200

    show volunteers

--- client/EventPageOrg.js
+++ client/EventPageOrg.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Text, View, Alert, TouchableOpacity, StyleSheet, Image,Button} from 'react-native';
+import { Text, View, Alert, TouchableOpacity, StyleSheet, Image,Button, FlatList} from 'react-native';
 import conf from '../config.js';
 export default class EventPageOrg extends React.Component {
   constructor(props) {
     super(props); 
         this.state = {
-        event: props.event,
-       
-        deleted:false
+          event: props.event,
+          tag: props.tag,
+          deleted:false,
+          showUsers:false,
+          data:[
+            {
+              age: 22,
+              createdAt: "2017-11-08T09:48:39.000Z",
+              email: "anas@humanity",
+              id: 1,
+              image:'https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/boy-512.png',
+              password: "$2a$10$zD2lyxGXD6QBVSxBR2BFPuUwjHhJPyMWWGemaGJrwkX8lpm3xT0rS",
+              rate: null,
+              updatedAt: "2017-11-08T09:48:39.000Z",
+              username: "anas",
+            },
+            {
+              age: 22,
+              createdAt: "2017-11-08T09:48:39.000Z",
+              email: "ammar@mm",
+              id: 1,
+              image:'https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/boy-512.png',
+              password: "$2a$10$zD2lyxGXD6QBVSxBR2BFPuUwjHhJPyMWWGemaGJrwkX8lpm3xT0rS",
+              rate: null,
+              updatedAt: "2017-11-08T09:48:39.000Z",
+              username: "Ammar",
+            }
+          ]
         };
   }
 
@@ -24,9 +49,8 @@ export default class EventPageOrg extends React.Component {
     })
  .then((response) => response.json())
       .then((data) => {
-   
        console.log(data)
-     
+       console.log(this.state.tag)
       this.setState({deleted : true})
        Alert.alert( 'DELETE ', data.message, [{text: 'OK', onPress: () => console.log('OK Pressed')}], { cancelable: true } )
       })
@@ -35,25 +59,58 @@ export default class EventPageOrg extends React.Component {
       });
   }
 
+  toggleVolunteers (){
+    this.setState({showUsers:!this.state.showUsers});
+  }
+
 
   render() {
     
 
-    if (!this.state.deleted) { 
+    if (!this.state.deleted && !this.state.showUsers) { 
       return ( <View>
        <Image source={require("../images/blue.jpg")} >
-        <TouchableOpacity >
         <Text>{this.state.event.name}</Text>
         <Text>{this.state.event.description}</Text>
         <Text>{this.state.event.location}</Text>
         <Text>{this.state.event.time}</Text> 
         <View style = {{flexDirection:'row', marginTop: 50,marginLeft:30}}>
         <Button title="Delete Event" onPress = {this.delete.bind(this)} />
-        <Text>               </Text>
+        <Text>      </Text>
+        <Button title="Show Volunteers" onPress = {this.toggleVolunteers.bind(this)} />
         </View>
-        </TouchableOpacity> 
         </Image>
      </View> )
+    } else if(!this.state.deleted && this.state.showUsers) {
+      return (
+        <View>
+       <Image source={require("../images/blue.jpg")} >
+        <Text>{this.state.event.name}</Text>
+        <Text>{this.state.event.description}</Text>
+        <Text>{this.state.event.location}</Text>
+        <Text>{this.state.event.time}</Text>
+        
+        <View style = {{flexDirection:'row', marginTop: 50,marginLeft:30}}>
+          <Button title="Hide Volunteers" onPress = {this.toggleVolunteers.bind(this)} />
+          <Text>      </Text>
+          <Button title="Delete Event" onPress = {this.delete.bind(this)} />
+          <Text>      </Text>
+        </View>
+        
+    <View style= {{ alignItems:"center",borderColor: 'black',marginTop: 30, borderRadius: 2,backgroundColor: '#87cefa'}}>
+       <Text style = {{marginTop: 20}}>Volunteers {'\n'}{'\n'}</Text>
+      </View>
+
+        <FlatList
+            data ={this.state.event.users}
+            renderItem ={({item}) =>  <TouchableOpacity onPress = {() => {this.props.showUserProfile(item)}} style= {{fontSize:20, alignContent:'center'}}><Text>{item.username}</Text></TouchableOpacity>
+            }
+           >
+        </FlatList>
+        </Image>
+     </View>
+
+        )
     } else {
       return null;
     }
--- client/UserProfileAsOrg.js
+++ client/UserProfileAsOrg.js
@@ -14,18 +14,16 @@ export default class UserProfile extends React.Component {
   render() {  
     return (
       <View>
-    
-    <View style= {{ alignItems:"center",borderColor: 'black', borderRadius: 2,backgroundColor: '#87cefa'}}>
-       <Text style = {{marginTop: 20}}>Welcome {this.state.information.username}{'\n'}{'\n'}</Text>
-      </View>
-      <Image source={require("../images/blue.jpg")} >
+    <Image source={require("../images/blue.jpg")} >
+      <Text style = {{marginTop: 20, marginLeft: 30,fontSize: 20,
+        fontWeight: 'bold',color:'white'}}>User Name:</Text>
+        <Text style = {{marginTop: 20,fontSize: 13, marginLeft: 50,color:'white' }}>{this.state.information.username}</Text>
       <Text style = {{marginTop: 20, marginLeft: 30,fontSize: 20,
         fontWeight: 'bold',color:'white'}}>Email:</Text>
         <Text style = {{marginTop: 20,fontSize: 13, marginLeft: 50,color:'white' }}>{this.state.information.email}</Text>
         <Text style = {{marginTop: 20, marginLeft: 30,fontSize: 20,
         fontWeight: 'bold',color:'white'}}>Rate:</Text>
         <Text style = {{marginTop: 20,color:'white',fontSize: 13, marginLeft: 50 }}>5</Text>
-
         <Text style = {{marginTop: 20, marginLeft: 30,fontSize: 20,
         fontWeight: 'bold',color:'white'}}>Phone Number:</Text>
         <Text style = {{marginTop: 20,color:'white',fontSize: 13, marginLeft: 50,fontSize: 13 }}>0798726360</Text>
@@ -35,7 +33,5 @@ export default class UserProfile extends React.Component {
   }
 
 }
-
-
 
 
